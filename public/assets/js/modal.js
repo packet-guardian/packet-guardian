3b4b96f1413c5ce2d213733f0cc61b9f75b5af82
Lee Keitel <lfkeitel@usi.edu>
2016-06-07 14:48:54 -0500

    Added custom confirmation and prompt windows
    Closes #1

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -13,28 +13,29 @@ function jsOverlay() {
 }
 
 function jsAlert(){
-    var cb = function(){};
-    var overlay = new jsOverlay();
-    var container = null;
+    this._okCallback = function(){};
+    this._overlay = new jsOverlay();
+    this._container = null;
 
 	this.show = function(dialog, callback){
-        cb = (callback) ? callback : function(){};
+        this._okCallback = (callback) ? callback : function(){};
 		var winW = window.innerWidth;
 	    var winH = window.innerHeight;
 	    var dialogbox = document.getElementById('dialogbox');
 
-        container = document.createElement("div");
+        this._container = document.createElement("div");
 
         var header = document.createElement("div");
         header.innerHTML = "Alert";
         $(header).addClass("js-modal-header");
-        container.appendChild(header);
+        $(header).prop("id", "js-modal-header-id");
+        this._container.appendChild(header);
 
 
         var body = document.createElement("div");
         body.innerHTML = dialog;
         $(body).addClass("js-modal-body");
-        container.appendChild(body);
+        this._container.appendChild(body);
 
         var footer = document.createElement("div");
         $(footer).addClass("js-modal-footer");
@@ -42,50 +43,53 @@ function jsAlert(){
         okButton.innerHTML = "OK";
         $(okButton).click(this.ok.bind(this));
         footer.appendChild(okButton);
-        container.appendChild(footer);
+        this._container.appendChild(footer);
 
-        $(container).addClass("js-modal");
-        document.body.insertBefore(container, document.body.firstChild);
-        $(container).style("left", (winW/2) - (500 * 0.5)+"px");
-        $(container).style("top", (winH/2) - (250 * 0.5)+"px");
+        var jContainer = $(this._container);
+        jContainer.addClass("js-modal");
+        document.body.insertBefore(this._container, document.body.firstChild);
+        jContainer.style("left", (winW/2) - (500 * 0.5)+"px");
+        jContainer.style("top", (winH/2) - (250 * 0.5)+"px");
 
-        overlay.show();
-        $(container).show();
+        this._overlay = new jsOverlay();
+        this._overlay.show();
+        jContainer.show();
 	};
 
 	this.ok = function(){
-        $(container).hide();
-        overlay.hide();
-        cb();
+        $(this._container).hide();
+        this._overlay.hide();
+        this._okCallback();
         document.body.removeChild(document.getElementsByClassName("js-modal")[0]);
 	};
 }
 
 function jsConfirm(){
-    var okCallback = function(){};
-    var cancelCallback = function(){};
-    var overlay = new jsOverlay();
-    var container = null;
+    this._okCallback = function(){};
+    this._cancelCallback = function(){};
+    this._overlay = null;
+    this._container = null;
 
     this.show = function(dialog, okCallback, cnlCallback){
-        okCallback = (okCallback) ? okCallback : function(){};
-        cancelCallback = (cnlCallback) ? cnlCallback : function(){};
+        this._okCallback = (okCallback) ? okCallback : function(){};
+        this._cancelCallback = (cnlCallback) ? cnlCallback : function(){};
         var winW = window.innerWidth;
         var winH = window.innerHeight;
         var dialogbox = document.getElementById('dialogbox');
 
-        container = document.createElement("div");
+        this._container = document.createElement("div");
 
         var header = document.createElement("div");
         header.innerHTML = "Confirm";
         $(header).addClass("js-modal-header");
-        container.appendChild(header);
+        $(header).prop("id", "js-modal-header-id");
+        this._container.appendChild(header);
 
 
         var body = document.createElement("div");
         body.innerHTML = dialog;
         $(body).addClass("js-modal-body");
-        container.appendChild(body);
+        this._container.appendChild(body);
 
         var footer = document.createElement("div");
         $(footer).addClass("js-modal-footer");
@@ -97,59 +101,63 @@ function jsConfirm(){
         $(cnlButton).click(this.cancel.bind(this));
         footer.appendChild(okButton);
         footer.appendChild(cnlButton);
-        container.appendChild(footer);
+        this._container.appendChild(footer);
 
-        $(container).addClass("js-modal");
-        document.body.insertBefore(container, document.body.firstChild);
-        $(container).style("left", (winW/2) - (500 * 0.5)+"px");
-        $(container).style("top", (winH/2) - (250 * 0.5)+"px");
+        var jContainer = $(this._container);
+        jContainer.addClass("js-modal");
+        document.body.insertBefore(this._container, document.body.firstChild);
+        jContainer.style("left", (winW/2) - (500 * 0.5)+"px");
+        jContainer.style("top", (winH/2) - (250 * 0.5)+"px");
 
         this._overlay = new jsOverlay();
-        overlay.show();
-        $(container).show();
+        this._overlay.show();
+        jContainer.show();
     };
 
     this.ok = function(){
-        $(container).hide();
-        overlay.hide();
-        okCallback();
+        $(this._container).hide();
+        this._overlay.hide();
+        console.log(this._okCallback);
+        this._okCallback();
         document.body.removeChild(document.getElementsByClassName("js-modal")[0]);
     };
 
     this.cancel = function(){
-        $(container).hide();
-        overlay.hide();
-        cancelCallback();
+        $(this._container).hide();
+        this._overlay.hide();
+        this._cancelCallback();
         document.body.removeChild(document.getElementsByClassName("js-modal")[0]);
     };
 }
 
 function jsPrompt(){
-    var okCallback = function(){};
-    var cancelCallback = function(){};
-    var overlay = new jsOverlay();
-    var container = null;
+    this._okCallback = function(){};
+    this._cancelCallback = function(){};
+    this._overlay = new jsOverlay();
+    this._container = null;
 
     this.show = function(dialog, okkCallback, cnlCallback){
-        okCallback = (okkCallback) ? okkCallback : function(){};
-        cancelCallback = (cnlCallback) ? cnlCallback : function(){};
+        this._okCallback = (okkCallback) ? okkCallback : function(){};
+        this._cancelCallback = (cnlCallback) ? cnlCallback : function(){};
         var winW = window.innerWidth;
         var winH = window.innerHeight;
         var dialogbox = document.getElementById('dialogbox');
 
-        container = document.createElement("div");
+        this._container = document.createElement("div");
 
         var header = document.createElement("div");
         header.innerHTML = "Prompt";
         $(header).addClass("js-modal-header");
-        container.appendChild(header);
+        $(header).addClass("grabbable");
+        $(header).prop("id", "js-modal-header-id");
+        this._container.appendChild(header);
 
 
         var body = document.createElement("div");
         body.innerHTML = dialog;
         body.innerHTML += "<form><input type=\"text\" id=\"js-modal-prompt-input\" size=\"50\"></form>";
         $(body).addClass("js-modal-body");
-        container.appendChild(body);
+        this._container.appendChild(body);
 
         var footer = document.createElement("div");
         $(footer).addClass("js-modal-footer");
@@ -161,28 +169,55 @@ function jsPrompt(){
         $(cnlButton).click(this.cancel.bind(this));
         footer.appendChild(okButton);
         footer.appendChild(cnlButton);
-        container.appendChild(footer);
+        this._container.appendChild(footer);
 
-        $(container).addClass("js-modal");
-        document.body.insertBefore(container, document.body.firstChild);
-        $(container).style("left", (winW/2) - (500 * 0.5)+"px");
-        $(container).style("top", (winH/2) - (250 * 0.5)+"px");
+        var jContainer = $(this._container);
+        jContainer.addClass("js-modal");
+        jContainer.prop("id", "js-modal-container");
+        document.body.insertBefore(this._container, document.body.firstChild);
+        jContainer.style("left", (winW/2) - (500 * 0.5)+"px");
+        jContainer.style("top", (winH/2) - (250 * 0.5)+"px");
 
-        overlay.show();
-        $(container).show();
+        this._overlay = new jsOverlay();
+        this._overlay.show();
+        jContainer.show();
+        document.getElementById("js-modal-prompt-input").focus();
+        bindMouseMove("#js-modal-header-id", "#js-modal-container");
     };
 
     this.ok = function(){
-        $(container).hide();
-        overlay.hide();
-        okCallback($("#js-modal-prompt-input").value());
+        $(this._container).hide();
+        this._overlay.hide();
+        this._okCallback($("#js-modal-prompt-input").value());
         document.body.removeChild(document.getElementsByClassName("js-modal")[0]);
     };
 
     this.cancel = function(){
-        $(container).hide();
-        overlay.hide();
-        cancelCallback();
+        $(this._container).hide();
+        this._overlay.hide();
+        this._cancelCallback();
         document.body.removeChild(document.getElementsByClassName("js-modal")[0]);
     };
+}
+
+function bindMouseMove(binderID, movediv) {
+    var binder = $(binderID);
+    console.log(binder);
+    binder.on("mousedown", function(e) {
+    	if (e.which !== 1) { return; }
+        var self = $(e.target);
+        var mover = $(movediv);
+        console.log(self);
+        self.style("position", "relative");
+
+        document.onmousemove = function(e) {
+            mover.style("left", e.pageX-250+'px');
+            mover.style("top", e.pageY-10+'px');
+        };
+    });
+    binder.on("mouseup", function() {
+    	document.onmousemove = null;
+    });
+
+    binder.on("dragstart", function() { return false; });
 }
