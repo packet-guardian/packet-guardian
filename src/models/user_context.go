3f7d810418b1fc8cf69dcfdccdb1069c6806f943
Lee Keitel <lfkeitel@usi.edu>
2016-09-28 15:33:00 -0500

    Updated sessions lib, removed pre Go1.7 contexts

--- src/models/user_context.go
+++ src/models/user_context.go
@@ -1,8 +1,6 @@
 // This source file is part of the Packet Guardian project.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-//
-// +build go1.7
 
 package models
 
